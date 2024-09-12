f75d44e69f0a6bc11b41d0f5073f804861c6e06d
“bojorquezjagit config --global user.name “bojorquezja <bojorquez.ja@gmail.com>
2024-09-12 10:04:59 +0200

    agrego for

--- repeticionfor/Program.cs
+++ repeticionfor/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int x = 1; x <=5; x = x + 1){
+    Console.WriteLine($"Hola {x}");
+}
+Console.WriteLine("FIN");
+
+//imprima 4,8,12,16
+for (int x = 4; x <= 16; x = x + 4) {
+    Console.Write($"{x},");
+}
---
+++ repeticionwhile/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Hello, World!");
+string re = "s";
+double suma = 0;
+double cant = 0;
+while (re =="s") {
+    Console.WriteLine("Desea participar (s/n)?");
+    re = Console.ReadLine().Trim().ToLower();
+    if (re == "s") {
+        Console.WriteLine("Ingrese edad");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        suma = suma + edad;
+        cant = cant + 1;
+    }
+}
+double prom = suma/cant;
+Console.WriteLine($"El promedio de {cant} edades es {prom}");
